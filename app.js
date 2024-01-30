6e2734a39fd50a088c6a4fe5370477c90b2202be
4mo <117049873+4m00@users.noreply.github.com>
2024-01-30 12:53:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,37 +10,30 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 
-
 btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Купить AirPods Pro 2");
-		item = "1";
-		tg.MainButton.show();
-	}
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+        tg.openPaymentForm({
+            payload: '1',  // Pass the item identifier as payload
+        });
+    }
+    else {
+        tg.MainButton.setText("Купить AirPods Pro 2");
+        item = "1";
+        tg.MainButton.show();
+    }
 });
 
 btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Купить AirPods 3");
-		item = "2";
-		tg.MainButton.show();
-	}
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+        tg.openPaymentForm({
+            payload: '2',  // Pass the item identifier as payload
+        });
+    }
+    else {
+        tg.MainButton.setText("Купить AirPods 3");
+        item = "2";
+        tg.MainButton.show();
+    }
 });
-
-
-
-
-
-
-
-
-
-
-
-
