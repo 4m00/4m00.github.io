64e12cfc1e5fea1e068dc64db4a455b4036350be
4mo <117049873+4m00@users.noreply.github.com>
2024-01-30 13:36:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -13,25 +13,27 @@ let btn2 = document.getElementById("btn2");
 btn1.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
+        // Send message to bot with selected item information
+        tg.sendMessage({ web_app_data: { data: "1" } });
+        // Close the Web App
         tg.close();
     } else {
         tg.MainButton.setText("Купить AirPods Pro 2");
         item = "1";
         tg.MainButton.show();
     }
-    // Send message to bot with selected item information
-    tg.sendMessage({ web_app_data: { data: item } });
 });
 
 btn2.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
+        // Send message to bot with selected item information
+        tg.sendMessage({ web_app_data: { data: "2" } });
+        // Close the Web App
         tg.close();
     } else {
         tg.MainButton.setText("Купить AirPods 3");
         item = "2";
         tg.MainButton.show();
     }
-    // Send message to bot with selected item information
-    tg.sendMessage({ web_app_data: { data: item } });
 });
