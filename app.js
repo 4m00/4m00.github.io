0950cd8020a3103b8d5d67bb5c7d3f3d4fd98fe5
4mo <117049873+4m00@users.noreply.github.com>
2024-01-30 23:49:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -5,43 +5,48 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
+let cart = [];
 let item = "";
 
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
+let cartBtn = document.getElementById("cartBtn");
+let cartCounter = document.getElementById("cartCounter");
 
 btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Приобрести AirPods Pro 2");
-		item = "1";
-		tg.MainButton.show();
-	}
+    toggleCartItem("AirPods Pro 2", 9000);
 });
 
 btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Приобрести AirPods 3");
-		item = "2";
-		tg.MainButton.show();
-	}
+    toggleCartItem("AirPods 3", 8000);
 });
+
+cartBtn.addEventListener("click", function(){
+    console.log(cart);
+});
+
+function toggleCartItem(productName, price) {
+    let existingItem = cart.find(item => item.name === productName);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({ name: productName, price: price, quantity: 1 });
+    }
+
+    updateCartCounter();
+}
+
+function updateCartCounter() {
+    let totalItems = cart.reduce((total, item) => total + item.quantity, 0);
+    cartCounter.innerText = totalItems > 0 ? totalItems : "";
+}
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
+    tg.sendData(item);
 });
 
-
 let usercard = document.getElementById("usercard");
-
 let p = document.createElement("p");
-
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
-
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 usercard.appendChild(p);
