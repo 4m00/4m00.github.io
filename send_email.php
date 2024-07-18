c882827f9c5b00db9c05703781ba38014fad69e2
4mo <117049873+4m00@users.noreply.github.com>
2024-07-18 21:35:33 +0300

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,24 @@
+<?php
+// Получаем значения переменных из пришедших данных
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+
+// Формируем сообщение для отправки, в нём мы соберём всё, что ввели в форме
+$mes = "Имя: $name \nE-mail: $email \nТелефон: $phone \nОписание: $message";
+
+// Пытаемся отправить письмо по заданному адресу
+$to = 'foto-master07@list.ru'; // адрес, на который будет отправлено письмо
+$subject = 'Новая заявка с сайта'; // тема письма
+$headers = "Content-type:text/plain; charset = UTF-8\r\nFrom:$email";
+
+$send = mail($to, $subject, $mes, $headers);
+
+// Если отправка прошла успешно — так и пишем
+if ($send == true) {
+    echo "Сообщение отправлено";
+} else {
+    echo "Ой, что-то пошло не так";
+}
+?>
